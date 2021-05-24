c07c35698534a97132308a0b07d575f76e41e92c
Abhiraj Shourya <45894478+abhirajshourya@users.noreply.github.com>
2021-05-25 01:07:03 +0530

    main

---
+++ Space Shooter/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Wave", menuName = "WaveConfig", order = 0)]
+public class WaveConfig : ScriptableObject 
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    [SerializeField] float spawnRandomFactor = 0.3f;
+    [SerializeField] int numberOfEnemies = 7;
+    [SerializeField] float moveSpeed = 2f;
+
+    public GameObject GetEnemyPrefab()
+    {
+        return enemyPrefab;
+    }
+    public GameObject GetPathPrefab()
+    {
+        return pathPrefab;
+    }
+
+    public float GetTimeBetweenSpawns()
+    {
+        return timeBetweenSpawns;
+    }
+
+    public float GetSpawnRandomFactor()
+    {
+        return spawnRandomFactor;
+    }
+
+    public int GetNumberOfEnemies()
+    {
+        return numberOfEnemies;
+    }
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+}
