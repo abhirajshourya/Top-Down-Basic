3505d3a1ea6a65b28a307b01bb15ddc12d60dae4
Abhiraj Shourya <45894478+abhirajshourya@users.noreply.github.com>
2021-05-29 23:13:44 +0530

    main

--- Space Shooter/Assets/Scripts/Enemy.cs
+++ Space Shooter/Assets/Scripts/Enemy.cs
@@ -5,22 +5,48 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float enemyHealth = 100f;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetShots = 0.2f;
+    [SerializeField] float maxTimeBetShots = 3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = Random.Range(minTimeBetShots, maxTimeBetShots);    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();    
     }
 
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if(shotCounter <= 0f)
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
-        
+
+        PrecessHit(damageDealer);
+    }
+
+    private void PrecessHit(DamageDealer damageDealer)
+    {
         enemyHealth -= damageDealer.GetDamage();
+
+        if (enemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
