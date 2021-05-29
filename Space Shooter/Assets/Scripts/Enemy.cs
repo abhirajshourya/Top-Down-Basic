22a0834fe7ea158ecaffa897da89cff750ba130a
Abhiraj Shourya <45894478+abhirajshourya@users.noreply.github.com>
2021-05-29 20:28:36 +0530

    main

---
+++ Space Shooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float enemyHealth = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        
+        enemyHealth -= damageDealer.GetDamage();
+    }
+}
